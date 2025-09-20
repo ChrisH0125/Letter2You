a3808d26b0d51737cac41d69325190b141734a17
Anakin Maksylewicz <anakin.maksylewicz@gmail.com>
2025-09-20 08:25:57 -0400

    made barebones letter writing page, text behind polaroid issue must be fixed or polaroid placed elsewhere

--- frontend/src/features/letters/LetterForm.tsx
+++ frontend/src/features/letters/LetterForm.tsx
@@ -1,8 +1,9 @@
 export default function LetterForm() {
+  // We'll probably need to return whatever's typed into the text area, because that's going to be the letter
   return (
-    <form className="p-2">
-      <label className="block">To</label>
-      <input className="border p-1 w-full" />
-    </form>
+      <form className="p-2 w-150 h-100 border rounded bg-[#2B1917]">
+        <textarea className="border p-7 w-full h-full resize-none" 
+        placeholder = "Write something sentimental :)"/>
+      </form>
   )
 }
--- node_modules/frontend/src/components/PolaroidPreview.tsx
+++ node_modules/frontend/src/components/PolaroidPreview.tsx
@@ -1,7 +1,15 @@
-export default function PolaroidPreview({ text }: { text: string }) {
+import littleGuy from '../assets/littleGuy.png'
+// placeholder image, remove when we figure out how to pass images
+export default function PolaroidPreview(){
   return (
-    <div className="bg-white p-4 shadow-md w-48"> 
-      <div className="text-sm">{text}</div>
+    <div className="bg-white w-30 h-35 p-2 flex flex-col items-center relative"> 
+      {/* six seven */}
+      <div className = "bg-black w-6/7 h-6/7 flex items-center justify-center">
+        {/* Bottom line is a placeholder for the image variable that'll be passed in*/}
+        <img src = {littleGuy} alt = "Attached image" className="pr-[1px] pl-[1px] pt-[1px] pb-[0px]  object-scale-down"/> 
+      </div>
+      {/* Replace the bottom placeholder text with the image description variable that will be passed in */}
+      <p className = "absolute text-[#2B1917] bottom-2 text-center text-xs">Image description </p>
     </div>
   )
 }
